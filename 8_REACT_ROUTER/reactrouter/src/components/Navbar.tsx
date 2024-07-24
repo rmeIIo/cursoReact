870fec322f596430365231c55e811983d8133362
rmeiio <j.rogercorreamello@gmail.com>
2024-07-24 20:42:17 -0300

    Link ativo

--- 8_REACT_ROUTER/reactrouter/src/components/Navbar.tsx
+++ 8_REACT_ROUTER/reactrouter/src/components/Navbar.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import './Navbar.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Navbar.css";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/About'>Sobre</Link>
+      <NavLink
+        to={"/"}
+        //</nav>className={({isActive}) => (isActive ? 'esta-ativo' : 'não-ativo')}
+      >
+        Home
+      </NavLink>
+      <NavLink to={"/about"}>Sobre</NavLink>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
