1fb4df2d43356e18d802099984f8b3a941d74006
rmeiio <j.rogercorreamello@gmail.com>
2024-07-24 20:36:32 -0300

    carregando dados, rotas dinâmicas, carregamento dinâmico de dados, nested routes, página 404

--- 8_REACT_ROUTER/reactrouter/src/App.jsx
+++ 8_REACT_ROUTER/reactrouter/src/App.jsx
@@ -4,16 +4,22 @@ import { BrowserRouter, Routes, Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
+import Product from "./pages/Product";
+import Info from "./pages/Info";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
-      <Navbar />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/products/:id" element={<Product />} />
+          <Route path="/products/:id/info" element={<Info />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ 8_REACT_ROUTER/reactrouter/src/pages/Product.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+
+const Product = () => {
+  const { id } = useParams();
+
+  const url = `http://localhost:3000/products/${id}`;
+  const { data: product, loading, error } = useFetch(url);
+
+  return (
+    <div>
+      <p>ID do produto: {id}</p>
+      {error && <p>Ocorreu um erro...</p>}
+      {loading && <p>Carregando...</p>}
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>R${product.price}</p>
+          <Link to={`/products/${product.id}/info`}>Mais informações</Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Product;
