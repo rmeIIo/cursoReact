9e99cafa2a1ef9059531ed8a8a3387f4d4db9da9
rmeiio <j.rogercorreamello@gmail.com>
2024-09-02 08:23:14 -0300

    useReducer com actions

--- 11_HOOKS/reacthooks/src/components/HookUseReducer.jsx
+++ 11_HOOKS/reacthooks/src/components/HookUseReducer.jsx
@@ -1,19 +1,70 @@
-import React from 'react'
-import { useReducer } from 'react'
+import React from "react";
+import { useReducer, useState } from "react";
 
 const HookUseReducer = () => {
   const [number, dispatch] = useReducer((state, action) => {
-    return Math.random(state)
-  })
+    return Math.random(state);
+  });
+
+  const initialTasks = [
+    { id: 1, title: "Estudar React" },
+    { id: 2, title: "Estudar Node" },
+    { id: 3, title: "Estudar React Native" },
+  ];
+
+  const taskReducer = (state, action) => {
+    switch (action.type) {
+      case "ADD":
+        const newTask = {
+          id: Math.random(),
+          title: taskText,
+        };
+
+        setTaskText("");
+
+        return [...state, newTask];
+      case "DELETE":
+        return state.filter((task) => task.id !== action.id);
+      default:
+        return state;
+    }
+  };
+
+  const [taskText, setTaskText] = useState("");
+  const [tasks, dispatchTask] = useReducer(taskReducer, initialTasks);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatchTask({ type: "ADD" });
+  };
+
+  const removeTask = (id) => {
+    dispatchTask({ type: "DELETE", id });
+  }
 
   return (
     <div>
       <h2>useReducer</h2>
       <p>Número: {number}</p>
       <button onClick={dispatch}>Alterar número!</button>
+
+      <h3>Tarefas:</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTaskText(e.target.value)}
+          value={taskText}
+        />
+
+        <input type="submit" value="Enviar" />
+      </form>
+      {tasks.map((task) => (
+        <li key={task.id} onDoubleClick={() => removeTask(task.id)}>{task.title}</li>
+      ))}
       <hr />
     </div>
-  )
-}
+  );
+};
 
-export default HookUseReducer
+export default HookUseReducer;
