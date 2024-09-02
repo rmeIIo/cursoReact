2bd69d61242fb64009d02f704a5471f9e5d4521e
rmeiio <j.rogercorreamello@gmail.com>
2024-09-02 08:13:13 -0300

    useReducer

--- 11_HOOKS/reacthooks/src/pages/Home.jsx
+++ 11_HOOKS/reacthooks/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import HookUseState from "../components/HookUseState";
+import HookUseReducer from "../components/HookUseReducer";
 
 const Home = () => {
   return (
     <div>
       <HookUseState />
+      <hr />
+      <HookUseReducer />
     </div>
   );
 };
